8432ea55c140e30e7794508e81682b3494bea6d2
= <=>
2018-05-06 12:39:20 -0700

    adding express projects

--- Js/JsFundamentals/pratice/learn.js
+++ Js/JsFundamentals/pratice/learn.js
@@ -6,6 +6,7 @@ setTimeout(function(){console.log(ninja+ " in 2nd style of call back fucntion");
 console.log(ninja+" out of function");
 function testcallback(){ console.log(ninja+ " in 1st style of call back fucntion"); }
 */
+/*
 var MyObjConstructor = function(name) {
     var myPrivateVar = "Hello"; // just to show that it is difficult to see this private var
     this.name = name; // but you can see the name!
@@ -16,3 +17,34 @@ var MyObjConstructor = function(name) {
   var obj1 = new MyObjConstructor('object1');
   var obj2 = new MyObjConstructor('object2');
   console.log(obj1);
+*/
+
+class Dot {
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+  }
+  showLocation() {
+      console.log(`This ${ this.constructor.name } is at x ${ this.x } and y ${ this.y }`);
+  }
+  // simple method in the parent class
+  parentFunction(){
+      return "This is coming from the parent!";
+  }
+}
+// child Circle class
+class Circle extends Dot {
+  constructor(x, y, radius) {
+      super(x, y);
+      this.radius = radius;
+  }
+  // simple function in the child class
+  childFunction() {
+      // by using super, we can call the parent method
+      const message = super.parentFunction();
+      console.log(message);
+  }
+}
+const circle = new Circle(1, 2, 3);
+//circle.childFunction();
+circle.parentFunction();
